135d3bd7900465a2efe575d8ba9803990804fedc
Marc <mdreem@fastmail.fm>
2019-11-20 23:20:22 +0100

    add scaling

--- test/clojureray/test_transformations.clj
+++ test/clojureray/test_transformations.clj
@@ -9,4 +9,36 @@
     (let [t (transformations/translation 5 -3 2)
           p [-3 4 5 1]]
       (is (= (matrix/multiply-vector t p) [[2.0] [1.0] [7.0] [1.0]]))))
+
+  (testing "Test inverse of a translation of point"
+    (let [t (matrix/invert (transformations/translation 5 -3 2))
+          p [-3 4 5 1]]
+      (is (= (matrix/multiply-vector t p) [[-8.0] [7.0] [3.0] [1.0]]))))
+
+  (testing "Test translation of vector"
+    (let [t (transformations/translation 5 -3 2)
+          v [-3 4 5 0]]
+      (is (= (matrix/multiply-vector t v) [[-3.0] [4.0] [5.0] [0.0]]))))
+  )
+
+(deftest multiply-by-scaling-matrix
+  (testing "Test scaling matrix applied to a point"
+    (let [s (transformations/scaling 2 3 4)
+          p [-4 6 8 1]]
+      (is (= (matrix/multiply-vector s p) [[-8.0] [18.0] [32.0] [1.0]]))))
+
+  (testing "Test applying an inverse scaling matrix applied to a point"
+    (let [s (matrix/invert (transformations/scaling 2 3 4))
+          p [-4 6 8 1]]
+      (is (= (matrix/multiply-vector s p) [[-2.0] [2.0] [2.0] [1.0]]))))
+
+  (testing "Test reflection of a point"
+    (let [s (matrix/invert (transformations/scaling -1 1 1))
+          p [2 3 4 1]]
+      (is (= (matrix/multiply-vector s p) [[-2.0] [3.0] [4.0] [1.0]]))))
+
+  (testing "Test scaling matrix applied to a vector"
+    (let [s (transformations/scaling 2 3 4)
+          v [-4 6 8 0]]
+      (is (= (matrix/multiply-vector s v) [[-8.0] [18.0] [32.0] [0.0]]))))
   )
