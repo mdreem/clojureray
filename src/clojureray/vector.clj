9e0c66f8dd3636d00e7e59720890520b32441106
Marc <mdreem@fastmail.fm>
2019-11-21 23:15:16 +0100

    make vector more general

--- src/clojureray/vector.clj
+++ src/clojureray/vector.clj
@@ -9,33 +9,38 @@
   (< (abs (- f1 f2)) 0.00001))
 
 (defn add
-  [[x1 x2 x3 w1] [y1 y2 y3 w2]]
-  [(+ x1 y1) (+ x2 y2) (+ x3 y3) (+ w1 w2)])
+  [v1 v2]
+  (mapv + v1 v2)
+  )
 
 (defn subtract
-  [[x1 x2 x3 w1] [y1 y2 y3 w2]]
-  [(- x1 y1) (- x2 y2) (- x3 y3) (- w1 w2)])
+  [v1 v2]
+  (mapv - v1 v2)
+  )
 
 (defn negate
-  [[x1 x2 x3 w1]]
-  [(- x1) (- x2) (- x3) (- w1)])
+  [v]
+  (mapv - v)
+  )
 
 (defn scalar-multiplication
-  [s [x1 x2 x3 w1]]
-  [(* s x1) (* s x2) (* s x3) (* s w1)])
-
-(defn squared-length
-  [[x1 x2 x3 w1]]
-  (+ (* x1 x1) (* x2 x2) (* x3 x3) (* w1 w1)))
-
-(defn length
-  [v1]
-  (Math/sqrt (squared-length v1)))
+  [s v]
+  (mapv (fn [e] (* s e)) v)
+  )
 
 (defn dot
   [v1 v2]
   (reduce + (mapv * v1 v2))
   )
+
+(defn squared-length
+  [v]
+  (dot v v)
+  )
+
+(defn length
+  [v1]
+  (Math/sqrt (squared-length v1)))
 
 (defn cross
   [[x1 x2 x3 _] [y1 y2 y3 _]]
