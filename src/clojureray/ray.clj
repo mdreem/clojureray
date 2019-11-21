46974a28c63a83b1d9dc070398918dd34fe77b4c
Marc <mdreem@fastmail.fm>
2019-11-22 00:12:49 +0100

    intersections with a sphere

--- src/clojureray/ray.clj
+++ src/clojureray/ray.clj
@@ -1,7 +1,22 @@
 (ns clojureray.ray
-  (:require [clojureray.vector :as vector]))
+  (:require [clojureray.vector :as vector]
+            [clojureray.comparison :refer :all]))
 
 (defn position
-  [[point vector] dist]
-  (vector/add point (vector/scalar-multiplication dist vector))
+  [[origin direction] dist]
+  (vector/add origin (vector/scalar-multiplication dist direction))
+  )
+
+(defn intersects
+  [origin direction]
+  (let [sphere_to_ray (vector/subtract origin [0.0 0.0 0.0 1.0])
+        a (vector/dot direction direction)
+        b (* 2 (vector/dot direction sphere_to_ray))
+        c (- (vector/dot sphere_to_ray sphere_to_ray) 1.0)
+        discriminant (- (* b b) (* 4 (* a c)))
+        discriminant_root (Math/sqrt discriminant)
+        t1 (/ (- (+ b discriminant_root)) (* 2 a))
+        t2 (/ (- (- b discriminant_root)) (* 2 a))]
+    (if (< discriminant 0) nil [t1 t2])
+    )
   )
--- test/clojureray/ray_test.clj
+++ test/clojureray/ray_test.clj
@@ -1,8 +1,9 @@
 (ns clojureray.ray-test
   (:require [clojure.test :refer :all]
-            [clojureray.ray :as ray]))
+            [clojureray.ray :as ray]
+            [clojureray.comparison :refer :all]))
 
-(deftest multiply-by-translation-matrix
+(deftest point-on-ray
   (let [point [2.0 3.0 4.0 1.0]
         vector [1.0 0.0 0.0 0.0]]
     (testing "Test point on ray at distance 0.0"
@@ -17,4 +18,21 @@
     (testing "Test point on ray at distance 2.5"
       (is (= (ray/position [point vector] 2.5) [4.5 3.0 4.0 1.0])))
     )
+  )
+
+(deftest intersections-with-sphere
+  (testing "A ray intersects a sphere at two points")
+  (is (aeq (ray/intersects [0.0 0.0 -5.0 1.0] [0.0 0.0 1.0 0.0]) [4.0 6.0]))
+
+  (testing "A ray intersects a sphere at a tangent")
+  (is (aeq (ray/intersects [0.0 1.0 -5.0 1.0] [0.0 0.0 1.0 0.0]) [5.0 5.0]))
+
+  (testing "A ray misses a sphere")
+  (is (= (ray/intersects [0.0 2.0 5.0 1.0] [0.0 0.0 1.0 0.0]) nil))
+
+  (testing "A ray originates inside a sphere")
+  (is (aeq (ray/intersects [0.0 0.0 0.0 1.0] [0.0 0.0 1.0 0.0]) [-1.0 1.0]))
+
+  (testing "A sphere is behind a ray")
+  (is (aeq (ray/intersects [0.0 0.0 5.0 1.0] [0.0 0.0 1.0 0.0]) [-6.0 -4.0]))
   )
